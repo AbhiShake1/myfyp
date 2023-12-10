32640c0bd4d55d34067a3e284866eb00e07376eb
abhishake1 <abhi9848038283@gmail.com>
2023-12-10 04:31:52 +0000

    feat: login and login-journey ui

---
+++ src/app/login/journey/page.tsx
@@ -0,0 +1,8 @@
+import { LoginJourneyForm } from "@/components/login-journey-form";
+import { Center } from "@/components/ui/center";
+
+export default function Page() {
+  return <Center>
+    <LoginJourneyForm />
+  </Center>
+}
---
+++ src/app/login/page.tsx
@@ -0,0 +1,8 @@
+import { LoginForm } from "@/components/login-form"
+import { Center } from "@/components/ui/center"
+
+export default function Page() {
+  return <Center>
+    <LoginForm />
+  </Center>
+}
