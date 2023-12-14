bcbea4a250d546c4ca2338b998d94528e7eb64da
abhishake1 <abhi9848038283@gmail.com>
2023-12-14 02:10:21 +0000

    enhancement: load onnetap js after the page is interactive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
               <TRPCReactProvider cookies={cookies().toString()}>
                 <SpeedInsights />
                 <Analytics />
-                <Script src="https://accounts.google.com/gsi/client" strategy="beforeInteractive" />
+                <Script src="https://accounts.google.com/gsi/client" strategy="afterInteractive" />
                 {children}
               </TRPCReactProvider>
             </body>
