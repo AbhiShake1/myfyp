944e888944c6d01e27314b3adb6170849044eb78
abhishake1 <abhi9848038283@gmail.com>
2023-12-23 04:28:39 +0000

    Revert "enhancement: schema-defined ui improvements"
    This reverts commit c0e16808419d00bebef400a5d07b24df8751acfa.

--- src/app/login/journey/page.tsx
+++ src/app/login/journey/page.tsx
@@ -1,78 +1,8 @@
-import { FypPage } from "@/components/dynamic";
+import { LoginJourneyForm } from "@/components/login-journey-form";
 import { Center } from "@/components/ui/center";
 
 export default function Page() {
   return <Center>
-    <FypPage>{{
-      head: {
-        title: "User Registration",
-        description: "Please enter your details to create an account",
-      },
-      body: {
-        sections: [
-          {
-            type: "card",
-            header: {
-              title: "Step 1: Personal Information",
-            },
-            inputs: [
-              {
-                title: "First Name",
-                placeholder: "John",
-                required: true,
-              },
-              {
-                title: "Last Name",
-                placeholder: "Doe",
-                required: true,
-              },
-            ],
-          },
-          {
-            type: "card",
-            header: {
-              title: "Step 2: College Selection",
-            },
-            inputs: [
-              {
-                title: "College",
-                placeholder: "Harvard University",
-              }
-            ],
-          },
-          {
-            type: "card",
-            header: {
-              title: "Step 3: Batch and Course Selection",
-            },
-            inputs: [
-              {
-                title: "Batch",
-                placeholder: "2022",
-                required: true,
-              },
-              {
-                title: "Course",
-                placeholder: "Computer Science",
-                required: true,
-              },
-            ],
-          },
-          {
-            type: "button",
-            text: "Register",
-            buttonType: "submit",
-            actions: [
-              {
-                navigate: {
-                  to: "/subscription/",
-                  type: "replace",
-                },
-              },
-            ],
-          },
-        ],
-      },
-    }}</FypPage>
+    <LoginJourneyForm />
   </Center>
 }
