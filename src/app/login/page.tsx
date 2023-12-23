baef0b2e13d7d7651614289429154c3bc92cbde0
abhishake1 <abhi9848038283@gmail.com>
2023-12-23 04:28:50 +0000

    Revert "enhancement: redirect and use schema for ui"
    This reverts commit e3995378599830a63a38749d45ee0168d83b39a9.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,7 @@
 import { LoginForm } from "@/components/login-form"
 import { Center } from "@/components/ui/center"
-import { getServerAuthSession } from "~/server/auth"
-import { redirect } from "next/navigation"
 
-export default async function Page() {
-  if ((await getServerAuthSession())?.user) {
-    return redirect("/");
-  }
-
+export default function Page() {
   return <Center>
     <LoginForm />
   </Center>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	mode: "jit",
   darkMode: ["class"],
   content: [
     '**/**/*.{ts,tsx}',
