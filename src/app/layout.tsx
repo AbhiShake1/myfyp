a9287bd11219b7b351b3ccfd32acc517c32e0547
abhishake1 <abhi9848038283@gmail.com>
2023-12-27 01:04:42 +0000

    feat: rich colors for toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
                   <ModeToggle />
                 </nav>
                 {children}
-                <Toaster />
+                <Toaster richColors />
               </TRPCReactProvider>
             </body>
           </OneTapSignin>
