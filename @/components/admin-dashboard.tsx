3d3dcd553828bab37ee797f8410e9ee1df5291f4
abhishake1 <abhi9848038283@gmail.com>
2023-12-28 14:26:20 +0000

    enhancement: show manage staff link only to admins

--- @/components/admin-dashboard.tsx
+++ @/components/admin-dashboard.tsx
@@ -1,26 +1,31 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import { getServerAuthSession } from "~/server/auth";
 
-export function AdminDashboard() {
+export async function AdminDashboard() {
+  const session = await getServerAuthSession();
+
   return <>
     <div className="flex items-center">
       <h1 className="font-semibold text-lg md:text-2xl">Welcome to Admin Dashboard</h1>
     </div>
     <div className="grid lg:grid-cols-3 gap-4">
-      <Card>
-        <CardHeader className="pb-4">
-          <CardTitle>Manage Staff</CardTitle>
-          <CardDescription>View, add, update, and delete staff members.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Link href="/admin/staffs">
-            <Button className="w-full" size="sm">
-              Go to Staff
-            </Button>
-          </Link>
-        </CardContent>
-      </Card>
+      {
+        session?.user.role === "admin" && <Card>
+          <CardHeader className="pb-4">
+            <CardTitle>Manage Staff</CardTitle>
+            <CardDescription>View, add, update, and delete staff members.</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Link href="/admin/staffs">
+              <Button className="w-full" size="sm">
+                Go to Staff
+              </Button>
+            </Link>
+          </CardContent>
+        </Card>
+      }
       <Card>
         <CardHeader className="pb-4">
           <CardTitle>Manage Students</CardTitle>
